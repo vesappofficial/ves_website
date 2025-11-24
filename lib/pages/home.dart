28f73b5ecb7b9504198485ca232499460c3b9f4f
marzc03 <marzbanc26@gmail.com>
2025-11-24 13:02:37 -0500

    Attempt 1 of fixing image displays

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -42,7 +42,7 @@ class HomeState extends State<Home> {
           minHeight: 95.vh,
           alignContent: AlignContent.center, 
           textAlign: TextAlign.center, 
-          backgroundImage: ImageStyle.url('/images/unrender.png'),
+          backgroundImage: ImageStyle.url('./web/images/unrender.png'),
           backgroundSize: BackgroundSize.cover,
           ),[
         h1([text('Welcome')]),
